fb8887d18999e64dbb5de015a181b56111e3c280
My_war <war@gmail.com>
2021-01-31 11:24:45 -0500

    Added comments to both the scripts.

--- module2.py
+++ module2.py
@@ -2,20 +2,28 @@
 
 import os
 import requests
+#Import the necessary modules required for the challenge.
+
 
 list_of_files = os.listdir("/data/feedback")
-#print(list_of_files)
+#list all the files and store in the variable.
 fields = ["title", "name", "date", "feedback"]
+#List of keys
 
 for file in list_of_files:
   with open("/data/feedback/{}".format(file)) as f:
     lines = f.read().strip().split("\n")
+    #Remove all the whitespace characters and store a list of all the lines in the variable.
     data_dict = {}
-    i = 0
+    #Initialize a empty dictionary
+    i = 0  #Counter variable
     for line in lines:
       data_dict[fields[i]] = line
       i += 1
+      #setting the content and saving it with the respective key.
     response = requests.post("http://35.239.53.9/feedback/", json=data_dict)
+    #Call the post method of the requests module and set the json attribute with the obtained dictionary.
     print(response.status_code)
   f.close()
+
 
